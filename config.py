a30bbfc3813c3dcccdc3f32941bfaa345f93637f
Прохоренко Софья <sofia.prokhorenko_1@mail.ru>
2025-12-26 16:10:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
-from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'BD_Kursach'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///time_tracker.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or ''
+    
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    
+    # КРИТИЧЕСКИ ВАЖНО: преобразовать postgres:// в postgresql://
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL or 'sqlite:///название.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = False
