e4c58fb110809fcd53e6ac555065eb1f351faeea
Darya Pronina <143608659+DashaPronina@users.noreply.github.com>
2025-12-26 13:05:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'BD_Kursach'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://time_tracker_user:gioz7F3J1k2WeaD6gwheZ3qSX7NZxgt8@dpg-d51cnqv5r7bs73flp8o0-a.oregon-postgres.render.com/time_tracker_i4rz'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///time_tracker.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
